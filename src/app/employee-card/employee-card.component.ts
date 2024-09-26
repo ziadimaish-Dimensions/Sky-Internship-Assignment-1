ca6d1e2d59917aa4d2d395c94cc9d053215e958b
Ziad Omaish <ziadomaish@dimensions-studio.com>
2024-09-26 22:32:35 +0300

    adding comments

--- src/app/employee-card/employee-card.component.ts
+++ src/app/employee-card/employee-card.component.ts
@@ -7,13 +7,16 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./employee-card.component.css'],
 })
 export class EmployeeCardComponent {
+  //input property to receive the employee object
   @Input() employee: any;
+  //input property to receive the search term
   @Input() searchTerm: string = '';
 
   // Function to highlight the matched term
   getHighlightedText(text: string, search: string) {
     if (!search) return text;
     const regex = new RegExp(`(${search})`, 'gi');
+    //highlight the text by wrapping it in a mark tag
     return text.replace(regex, '<mark>$1</mark>');
   }
 }
--- src/app/highlight.pipe.ts
+++ src/app/highlight.pipe.ts
@@ -5,6 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class HighlightPipe implements PipeTransform {
+  // filters the employees based on the search term
   transform(employees: any[], searchTerm: string): any[] {
     if (!searchTerm) return employees;
 
