fe4b0c7d5bb7a2b2467c36f85f56621842fcc5f2
Ziad Omaish <ziadomaish@dimensions-studio.com>
2024-09-26 22:23:13 +0300

    adding highlight feature

--- src/app/employee-card/employee-card.component.ts
+++ src/app/employee-card/employee-card.component.ts
@@ -8,4 +8,12 @@ import { Component, Input } from '@angular/core';
 })
 export class EmployeeCardComponent {
   @Input() employee: any;
+  @Input() searchTerm: string = '';
+
+  // Function to highlight the matched term
+  getHighlightedText(text: string, search: string) {
+    if (!search) return text;
+    const regex = new RegExp(`(${search})`, 'gi');
+    return text.replace(regex, '<mark>$1</mark>');
+  }
 }
